83d2b6927ddb3a98e655d3d175898b04f81499d6
ural <sevomax@gmail.com>
2018-03-01 21:54:40 -0500

    playing with stuff

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends PureComponent {
         { id: 'asdf11', name: 'Stephanie', age: 26 }
       ],
       otherState: 'some other STATE value',
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
   }
 
@@ -80,7 +81,13 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
+    this.setState((prevState, props) => {
+        return {
+          showPersons: !doesShow,
+          toggleClicked: prevState.toggleClicked +1
+        }
+      }
+    );
   }
 
   render () {
@@ -101,11 +108,12 @@ class App extends PureComponent {
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          clicked={this.togglePersonsHandler} />
+          clicked={this.togglePersonsHandler}
+          howManyTimes={this.state.toggleClicked}
+        />
         {persons}
       </WithClass>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
